f9ae1abab20b1f70aa0b6776cde094de4486ee0f
Christopher David <chris@arcade.city>
2023-01-27 16:40:14 -0600

    Add getBlankEvent test

--- tests/unit/nostr/nostr.test.ts
+++ tests/unit/nostr/nostr.test.ts
@@ -1,6 +1,21 @@
 /**
  * This file contains a set of unit tests for nostr related functions like getBlankEvent(),
- * serializeEvent(), getEventHash(), validateEvent(), verifySignature() and signEvent(). It tests these
- * functions with an array of filters and an event object as parameters to check if any of them match
- * each other or not.
+ * serializeEvent(), getEventHash(), validateEvent(), verifySignature() and signEvent().
  */
+
+import { expect } from 'chai'
+import { getBlankEvent } from '../../../src/nostr/event'
+
+describe('getBlankEvent', () => {
+  it('should return an empty event object with properties for kind, pubkey, content, tags and created_at', () => {
+    const blank = getBlankEvent()
+
+    expect(blank).to.have.property('kind')
+    expect(blank).to.have.property('pubkey')
+    expect(blank).to.have.property('content')
+    expect(blank).to.have.property('tags')
+    expect(blank).to.have.property('created_at')
+
+    // TODO: Add more assertions here to check the types of each property value in the blank event object returned by the function
+  })
+})
