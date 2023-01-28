d00bbfb4a00c7a4067745fef3b77569d775d76fa
Christopher David <chris@arcade.city>
2023-01-27 18:50:08 -0600

    Add initial nostr tests

--- tests/unit/nostr/nostr.spec.ts
+++ tests/unit/nostr/nostr.spec.ts
@@ -4,7 +4,14 @@
  */
 
 import { expect } from 'chai'
-import { getBlankEvent } from '../../../src/nostr/event'
+import {
+  getBlankEvent,
+  serializeEvent,
+  getEventHash,
+  validateEvent,
+  verifySignature,
+  signEvent,
+} from '../../../src/nostr/event'
 
 describe('getBlankEvent', () => {
   it('should return an empty event object with properties for kind, pubkey, content, tags and created_at', () => {
@@ -15,7 +22,65 @@ describe('getBlankEvent', () => {
     expect(blank).to.have.property('content')
     expect(blank).to.have.property('tags')
     expect(blank).to.have.property('created_at')
+    expect(typeof blank['kind'] === 'number').to.be.true // Passes because kind is a number as expected
+    expect(typeof blank['pubkey'] === 'object').to.be.true // Passes because pubkey is null as expected so its type is "object" according to JavaScript's typeof operator
+    expect(typeof blank['content'] === 'string').to.be.true // Passes because content is an empty string as expected so its type is "string" according to JavaScript's typeof operator
+    expect(Array.isArray(blank['tags'])).to.be.true // Passes because tags is an empty array as expected
+  })
+})
 
-    // TODO: Add more assertions here to check the types of each property value in the blank event object returned by the function
+describe('serializeEvent', () => {
+  it("should return a JSON string representing the event's data", () => {
+    let evt = getBlankEvent()
+
+    let jsonString = serializeEvent(evt)
+
+    let parsedJson = JSON.parse(jsonString)
+    expect(parsedJson).to.have.lengthOf(6)
+    expect(parsedJson[0]).to.equal(0)
+    expect(parsedJson[1]).to.equal(null)
+    expect(parsedJson[2]).to.equal(0)
+    expect(parsedJson[3]).to.equal(255)
+    expect(parsedJson[4]).to.deep.equal([])
+    expect(parsedJson[5]).to.equal('')
+  })
+})
+
+describe('getEventHash', () => {
+  it("should return a SHA256 hash of the event's data", () => {
+    let evt = getBlankEvent()
+    let hash = getEventHash(evt)
+
+    // Check that hash is a hexadecimal string 32 characters long (64 characters total including "0x")
+    expect(hash).to.be.a('string')
+    expect(hash).to.have.lengthOf(64)
+  })
+})
+
+describe('validateEvent', () => {
+  it('should return true if all fields are valid', () => {
+    let evt = getBlankEvent()
+    let result = validateEvent(evt)
+
+    // Check that result is a boolean value
+    expect(typeof result === 'boolean').to.be.true
+
+    // Check that result is false when given an invalid event object as input
+    evt = getBlankEvent()
+    expect(validateEvent(evt)).to.be.false
+  })
+})
+
+describe('verifySignature', () => {
+  it('should return true if the signature is valid', async () => {
+    let evt = getBlankEvent()
+    let result = await verifySignature(evt)
+
+    // Check that result is a boolean value
+    expect(typeof result === 'boolean').to.be.true
+
+    // Check that result is false when given an invalid event object as input
+    evt = getBlankEvent()
+    expect(validateEvent(evt)).to.be.false
   })
 })
